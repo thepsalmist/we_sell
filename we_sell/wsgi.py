5483d5e9aaf05a444d8f5f4bc2f715242a5d9f49
thepsalmist <xf.xavierfrank@gmail.com>
2020-05-18 08:52:04 +0300

    initial commit

--- we_sell/wsgi.py
+++ we_sell/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for new_project project.
+WSGI config for we_sell project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'we_sell.settings')
 
 application = get_wsgi_application()
